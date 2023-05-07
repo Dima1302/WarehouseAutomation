f52e0be0eede0fbb64485209de9220bb61637f56
Dmitriy <Dimitriy303@yandex.ru>
2023-05-07 21:14:28 +0900

    Initial commit

---
+++ src/main/java/ru/skypro/myproject/WarehouseAutomation/WarehouseAutomationProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.myproject.WarehouseAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseAutomationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseAutomationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/myproject/WarehouseAutomation/WarehouseAutomationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.myproject.WarehouseAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseAutomationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
